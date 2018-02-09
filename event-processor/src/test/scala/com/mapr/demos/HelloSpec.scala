4b1e780881d23d3b0057fd9611b95e59c2c5c5de
Vladislav Glinskiy <vglinskiy@cybervisiontech.com>
2018-02-09 11:58:08 +0200

    issue #2: Event-processor project added

---
+++ event-processor/src/test/scala/com/mapr/demos/HelloSpec.scala
@@ -0,0 +1,9 @@
+package com.mapr.demos
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
